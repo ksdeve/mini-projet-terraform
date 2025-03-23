e6d78a4ed53606558b7960f6ee3da11e1a2f97ab
ksdeve <salmon.kevin.pro@gmail.com>
2025-03-23 22:34:40 +0100

    add description in variable

--- variables.tf
+++ variables.tf
@@ -1,54 +1,64 @@
 variable "resource_group_name" {
-  default = "FlaskRG"
+  description = "Le nom du groupe de ressources Azure à créer"
+  default     = "FlaskRG"
 }
 
 variable "location" {
-  default = "East US"
+  description = "La région Azure où les ressources seront déployées"
+  default     = "East US"
 }
 
 variable "vm_name" {
-  default = "FlaskVMKevinS"
+  description = "Le nom de la machine virtuelle"
+  default     = "FlaskVMKevinS"
 }
 
 variable "vm_size" {
-  default = "Standard_B2s"  # 2 CPU, 4 Go RAM
+  description = "La taille de la machine virtuelle (par exemple, nombre de CPU, RAM)"
+  default     = "Standard_B2s"  # 2 CPU, 4 Go RAM
 }
 
-
-
 variable "admin_username" {
-  default = "azureuser"
+  description = "Le nom d'utilisateur administrateur pour la machine virtuelle"
+  default     = "azureuser"
 }
 
 variable "admin_password" {
-  description = "Password for the VM admin user"
+  description = "Mot de passe pour l'utilisateur administrateur de la machine virtuelle"
   sensitive   = true
 }
+
 variable "storage_account_name" {
-  default = "flaskstorageacct"
+  description = "Le nom du compte de stockage"
+  default     = "flaskstorageacct"
 }
 
 variable "container_name" {
-  default = "flaskcontainer"
+  description = "Le nom du conteneur Blob dans le compte de stockage"
+  default     = "flaskcontainer"
 }
 
 variable "db_name" {
-  default = "flaskdb"
+  description = "Le nom de la base de données PostgreSQL"
+  default     = "flaskdb"
 }
 
 variable "db_username" {
-  default = "flaskadmin"
+  description = "Le nom d'utilisateur administrateur de la base de données PostgreSQL"
+  default     = "flaskadmin"
 }
 
 variable "db_password" {
-  description = "Password for the PostgreSQL admin user"
+  description = "Mot de passe pour l'utilisateur administrateur de la base de données PostgreSQL"
   sensitive   = true
 }
+
 variable "db_version" {
-  default = "11"  # PostgreSQL version
+  description = "La version de PostgreSQL à utiliser"
+  default     = "11"  # Version de PostgreSQL
 }
 
 variable "subscription_id" {
-  description = "Azure Subscription ID"
+  description = "L'ID de l'abonnement Azure"
   type        = string
 }
